8393227b175b0f443fd2860166b4fe759d92099b
Hironao OTSUBO <motemen@gmail.com>
2023-05-23 01:44:00 +0900

    wip

--- api/ogimage.tsx
+++ api/ogimage.tsx
@@ -4,16 +4,15 @@ export const config = {
   runtime: "edge",
 };
 
-const image = fetch(
-  new URL("../public/kanjivg/0f9a8.svg", import.meta.url)
-).then((res) => res.arrayBuffer());
 export default async function handler(request: Request) {
-  const imageData = await image;
-
   return new ImageResponse(
     (
       <div>
-        <img width="256" height="256" src={imageData} />
+        <img
+          width="256"
+          height="256"
+          src={"https://kanji-strokes-puzzle.vercel.app/kanjivg/0672c.svg"}
+        />
       </div>
     )
   );
